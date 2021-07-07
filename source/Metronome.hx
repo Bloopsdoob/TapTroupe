87a0626fdbdf84649079b5d405a78ea3ae601640
Bloopsdoob <77730957+Bloopsdoob@users.noreply.github.com>
2021-07-07 17:44:59 +1000

    FIXED EVERYTHING LOL

--- source/Metronome.hx
+++ source/Metronome.hx
@@ -18,13 +18,16 @@ class Metronome
 	public static var readyCount:Int = 0;
 	public static var polyCount:Int = 0;
 	public static var polyCountStart:Int = 0;
+	
+	public static var onOffBeat:Bool = false;
+	public static var polyOffset:Int = 0;
+	public static var polyTapOffset:Int = 0;
 		
 	public static function metronomeUpdate():Void
 	{
 		oldStep = curStep;
 		curStep = Math.floor(songPosition / stepCrochet);
 		curBeat = Math.floor(curStep / 4);
-		curBeatPoly = Math.floor(curStep / 3);
 		
 		if (oldStep != curStep && curStep > 0)
 			stepHit();
@@ -33,15 +36,19 @@ class Metronome
 	{
 		if (curStep % 4 == 0)
 			beatHit();
-		if (curStep % 3 == 0)
+			
+		if ((curStep % 3 == polyOffset) && (polyCount > 1))
 			beatHitPoly();
+			
+		if ((curStep % 4 == 2) && (onOffBeat))
+			offBeatHit();
 	}
 	public static function beatHitPoly():Void
 	{
+		polyTapOffset = if((polyCountStart % 2 == 1) && (!(polyCount < 4))) 1 else 0;
 		if (polyCount > 1)
 		{
-
-			if (polyCount % 2 == 0)
+			if (polyCount % 2 == polyTapOffset)
 			{
 				if (polyCount >= 4)
 				{
@@ -74,12 +81,52 @@ class Metronome
 
 		}		
 	}
-
+	public static function offBeatHit():Void
+	{
+		polyCount = polyCountStart; //lol
+		polyOffset = (curStep % 3);
+		onOffBeat = false;
+		beatHitPoly();
+	}
+	public static function startPoly(polyDuration:Int)
+	{
+		FlxG.sound.play(AssetPaths.tapReady1__ogg, 1, false);
+		readyCount = 1;
+		polyCountStart = polyDuration;
+	}
+	public static function polyUpdate():Void
+	{
+		readyCount++;
+		switch (readyCount)
+		{
+			case 2:
+				FlxG.sound.play(AssetPaths.tapReady2__ogg, 1, false);
+			case 3:
+				FlxG.sound.play(AssetPaths.tapAnd2__ogg, 1, false);
+				onOffBeat = true;
+		}
+	}
 	public static function beatHit():Void
 	{
-		if (readyCount == 4)
+		if (readyCount > 0)
+			polyUpdate();
+		else if (readyCount == 4)
 			readyCount = 0;
-			
+		
+		if ((polyCount == 1) && (curBeat % 4 == 0))
+		{
+			FlxG.sound.play(AssetPaths.tapEnd__ogg, 1, false);
+			for (i in 0...3)
+			{
+				var step:FlxSound = FlxG.sound.load(AssetPaths.tapStep2__ogg);
+				step.pan = ((-0.5) + (i*0.5));
+				step.volume = 0.50;
+				step.play();
+			}
+			polyCount = 0;
+			readyCount = 4;
+		}
+		
 		if (curBeat == 4)
 		{
 			FlxG.sound.play(AssetPaths.tapEnd__ogg, 1, false);
@@ -94,35 +141,16 @@ class Metronome
 				step.play();
 			}
 		}
-		if (curBeat == 14)
+		switch (curBeat)
 		{
-			FlxG.sound.play(AssetPaths.tapReady1__ogg, 1, false);
-			readyCount = 1;
-		}
-		if (curBeat == 15)
-		{
-			FlxG.sound.play(AssetPaths.tapReady2__ogg, 1, false);
-			readyCount = 2;
-		}
-		if (curBeat == 16)
-		{
-			FlxG.sound.play(AssetPaths.tapAnd2__ogg, 1, false);
-			readyCount = 3;
-			polyCount = 6;
-			polyCountStart = 6;
-		}
-		if ((polyCount == 1) && (curBeat % 4 == 0))
-			{
-			FlxG.sound.play(AssetPaths.tapEnd__ogg, 1, false);
-			for (i in 0...3)
-			{
-				var step:FlxSound = FlxG.sound.load(AssetPaths.tapStep2__ogg);
-				step.pan = ((-0.5) + (i*0.5));
-				step.volume = 0.50;
-				step.play();
-			}
-			polyCount = 0;
-			readyCount = 4;
+			case 14|46|78|190|222:
+				startPoly(6);
+			case 32|64|116|124|132|140|148|152|176|208:
+				startPoly(3);
+			case 156:
+				startPoly(8);
+			case 90|106:
+				startPoly(11);
 		}
 	}
 }
